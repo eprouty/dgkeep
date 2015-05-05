528ee484f97766253722cfc83fc2cebb851f4be8
Eric Prouty <Eric.Prouty@viasat.com>
2015-05-05 11:28:53 -0400

    Adding support for connecting to a postgresql database via Sequelize

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 [![Coverage Status](https://coveralls.io/repos/eprouty/dgkeep/badge.svg?branch=master)](https://coveralls.io/r/eprouty/dgkeep?branch=master)
 
 Configurable API for tracking game scores
+
+## Environment
+This project uses NPM to manage its dependencies, to set up your development environment run `npm install`
+
+This project using Sequalize as an RDBMS so it can handle most relational databases, the test suite is run against a PostgreSQL backend so that must be setup as well.
+
+```bash
+brew install postgresql
+initdb /usr/local/var/postgres -E utf8
+mkdir -p ~/Library/LaunchAgents
+cp /usr/local/Cellar/postgresql/9.4.1/homebrew.mxcl.postgresql.plist ~/Library/LaunchAgents/
+launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+createdb dgkeep_test
+psql -c 'CREATE ROLE postgres LOGIN;' dgkeep_test
+```
--- test/lib/test_dgkeep.js
+++ test/lib/test_dgkeep.js
@@ -1,11 +1,44 @@
 'use strict';
 
 var dgkeep = require('../../lib/dgkeep');
+
 var winston = require('winston');
 var logger = new winston.Logger({transports: [new winston.transports.Console({level:'silly',timestamp:true,colorize:true})]});
+var assert = require('assert');
+var _ = require('lodash');
+var Q = require('q');
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('dgkeep_test', 'postgres', null,
+                              {dialect: "postgres",
+                               port: 5432});
 
 describe('dgkeep', function(){
-    it ('Should be able to initialize', function(){
-        dgkeep(logger);
+    describe('Setup', function(){
+        it ('Should be able to initialize', function(){
+            dgkeep(logger);
+        });
+
+        it ('Should return an error if the database can\'t connect', function(done){
+            var dummySequelize = {authenticate: function(){
+                var defer = Q.defer();
+                defer.reject(new Error('Failed Connection'));
+                return defer.promise.nodeify();
+            }};
+            dgkeep(logger, dummySequelize, function(err){
+                if (err){
+                    assert(err);
+                } else {
+                    assert.fail();
+                }
+                done();
+            });
+        });
+
+        it ('Should be able to connect to the pgsql test database', function(done){
+            assert.doesNotThrow(function(){
+                dgkeep(logger, sequelize, done);
+            });
+        });
     });
 });
